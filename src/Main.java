547ca3d2f586b1785e188b6578db4db3a165a4e7
Tagir <amon.ra@inbox.ru>
2020-10-07 18:29:45 +0300

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        int button;
+        Scanner x = new Scanner(System.in);
+
+        while (true){
+            button = x.nextInt();
+
+            if (button == 1) {
+                System.out.println("Language selection");
+            } else if (button == 2) {
+                System.out.println("Customer support");
+            } else if (button == 3) {
+                System.out.println("Check the balance");
+            } else if (button == 4) {
+                System.out.println("Check the load balance");
+            } else if (button == 0) {
+                System.out.println("Exit");
+                break;
+            }
+        };
+    }
+}
